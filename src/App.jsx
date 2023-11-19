5e252d0ca88ea7e27e794b37c76b107500f93efa
isha-web1 <isha.web1@gmail.com>
2023-11-19 22:52:52 +0600

    button-1 added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold mt-7">Animation Buttons</h1>
+      <p className="text-4xl font-bold mt-7">Hover us and enjoy the satisfying neumorphic animation designs!</p>
+      <div className="frame">
+      <button class="custom-btn btn-1 ">Read More</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
