d78af5070249cee7204963ea61a670ad03594c63
isha-web1 <isha.web1@gmail.com>
2023-11-19 23:09:48 +0600

    button-2 added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <p className="text-4xl font-bold mt-7">Hover us and enjoy the satisfying neumorphic animation designs!</p>
       <div className="frame">
       <button class="custom-btn btn-1 ">Read More</button>
+      <button class="custom-btn btn-2 ">Read More</button>
       </div>
     </>
   );
